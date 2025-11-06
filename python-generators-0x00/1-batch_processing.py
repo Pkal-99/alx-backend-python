9945758f37b681ccfe446b63dfb6b6a719901b94
Pkal-99 <kal4ever10q@gmail.com>
2025-11-06 08:55:42 +0300

    update generator to fetch and process data in batches from the users database

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,7 +2,7 @@
 # Batch process
 import seed
 def stream_users_in_batches(batch_size):
-    """Yield users in batches of given size."""
+    
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
 
@@ -24,3 +24,4 @@ def batch_processing(batch_size):
         for user in batch:
             if int(user['age']) > 25:
                 print(user)
+    return user
