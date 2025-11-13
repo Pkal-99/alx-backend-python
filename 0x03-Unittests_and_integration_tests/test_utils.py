6822a5fb7aa33735f9d938c4cad563345e212677
Pkal-99 <kal4ever10q@gmail.com>
2025-11-13 12:49:53 +0300

    update1 test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,14 +7,15 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ class that inherits from unittest.TestCase     """
+    """ class that inherits from unittest.TestCase """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Method to test that the method returns what it is supposed to expected. """
+        """ Method to test that the method returns what it is
+            supposed to expected. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -74,4 +75,3 @@ class TestMemoize(unittest.TestCase):
             spec.a_property
             spec.a_property
             mocked.asset_called_once()
-            
