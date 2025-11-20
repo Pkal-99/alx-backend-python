46621170f25eedf6d88004f2b4f3a11f2c18e174
Pkal-99 <kal4ever10q@gmail.com>
2025-11-20 19:36:57 +0300

    updated setting py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,6 +87,12 @@ REST_FRAMEWORK = {
           
     ],
     # ... other DRF settings if needed ...
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',  # Enable filtering
+        'rest_framework.filters.SearchFilter',  # Optional: enable search
+        'rest_framework.filters.OrderingFilter',  # Optional: enable ordering
 }
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
