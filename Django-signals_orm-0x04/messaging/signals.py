bed90ae284d7e53db88ec89e72827b3880a196cb
Pkal-99 <kal4ever10q@gmail.com>
2025-11-28 12:50:14 +0300

    update signal log message edit

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+from django.db.models.signals import pre_save
+
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+        
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # Ensure that the instance already exists in the database
+        old_instance = sender.objects.get(pk=instance.pk)
+        if old_instance.content != instance.content:  # Check if content has changed
+            MessageHistory.objects.create(message=old_instance, old_content=old_instance.content)
+            instance.edited = True  # Mark the message as edited
