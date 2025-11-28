a55d686ecb4e6c4ca78a1862ca987063c5addb5e
Pkal-99 <kal4ever10q@gmail.com>
2025-11-28 17:14:07 +0300

    updated views

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Message
+from django.views.decorators.cache import cache_page
 
 
 #@login_required
@@ -62,3 +63,15 @@ def inbox_view(request):
 def inbox(request):
     unread_messages = Message.unread.unread_for_user(request.user).select_related('sender').only('content', 'timestamp', 'sender__username')
     return render(request, 'messaging/inbox.html', {'messages': unread_messages})
+
+"""# Cache for datat retrival
+@login_required
+@cache_page(60 * 1)  # Cache the view for 60 seconds
+def conversation_view(request, message_id):
+    # Fetch the conversation messages and replies using the custom manager
+    message = get_object_or_404(Message.objects.select_related('sender', 'receiver').prefetch_related('replies'), id=message_id)
+    
+    # If you're retrieving more messages related to the conversation, fetch them here as well
+
+    return render(request, 'messaging/conversation.html', {'message': message})
+"""
