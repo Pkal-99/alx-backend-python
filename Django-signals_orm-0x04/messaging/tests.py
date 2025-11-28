9efb1fa4df795fd2e085fd49a414e919283f660b
Pkal-99 <kal4ever10q@gmail.com>
2025-11-28 12:51:03 +0300

    update test with message edit test case

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 class NotificationTestCase(TestCase):
     def setUp(self):
@@ -12,3 +12,28 @@ class NotificationTestCase(TestCase):
         notification = Notification.objects.get(message=message)
         self.assertEqual(notification.user, self.receiver)
         self.assertEqual(notification.message, message)
+        
+class MessageEditTestCase(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username='sender', password='12345')
+        self.receiver = User.objects.create_user(username='receiver', password='12345')
+        self.message = Message.objects.create(sender=self.sender, receiver=self.receiver, content='Initial Message')
+
+    def test_message_edit_logging(self):
+        # Edit the message
+        self.message.content = 'Edited Message'
+        self.message.save()  # Triggers the pre_save signal
+        
+        # Check that MessageHistory is created
+        history = MessageHistory.objects.get(message=self.message)
+        self.assertEqual(history.old_content, 'Initial Message')
+        self.assertEqual(self.message.edited, True)  # Ensure the edited field is set to True
+
+    def test_view_message_history(self):
+        self.message.content = 'Edited Message'
+        self.message.save()  # Create history record
+        
+        # Verify history can be accessed through the get_history method
+        histories = self.message.get_history()
+        self.assertEqual(histories.count(), 1)
+        self.assertEqual(histories.first().old_content, 'Initial Message')
